e10918bddf3a643ff464bea5a3992acfcc1d065b
bryanagamk <bryan@kyodo-i.com>
2021-06-30 06:36:19 +0700

    channel sbg parameter

--- channel_test.go
+++ channel_test.go
@@ -20,3 +20,19 @@ func TestCreateChannel(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	defer close(channel)
 }
+
+func GiveMeReponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Brooo"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+
+	go GiveMeReponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+	defer close(channel)
+}
