9a44ea2092a8cfd263f7e1c78320df08af298b04
bryanagamk <bryan@kyodo-i.com>
2021-06-30 07:09:24 +0700

    Default Select Channel

--- channel_test.go
+++ channel_test.go
@@ -114,10 +114,35 @@ func TestSelectChannel(t *testing.T) {
 			fmt.Println("Data dari Channel 2", data)
 			counter++
 		}
-
 		if counter == 2 {
 			break
 		}
 	}
+}
 
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeReponse(channel1)
+	go GiveMeReponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
 }
